e337b07d5a250fbefe3bcef0c6a31a615e461105
Tirupati Balan <tirupatibalan@Tirupatis-MacBook-Air.local>
2019-04-17 11:47:32 +0530

    Setup base view controller

--- Contacts/Classes/GKLibrary/GKView/GKBasicViewController.swift
+++ Contacts/Classes/GKLibrary/GKView/GKBasicViewController.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import UIKit
+
+protocol DismissProtocol {
+    func dismiss(_ sender : Any)
+}
+
+protocol PopViewProtocol {
+    func popView(_ sender : Any)
+}
+
+class GKBasicViewController : UIViewController {
+    var dismissProtocol : DismissProtocol!
+    var popViewProtocol : PopViewProtocol!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func dismiss() {
+        if ((self.dismissProtocol) != nil) {
+            self.dismissProtocol.dismiss(self)
+        }
+    }
+    
+    func dismissSelf() {
+        self.dismiss(animated: true) { //Trying to dismiss from default dismiss func of view controller
+            self.dismiss() //If not then trying to look into via calling dismiss func of DismissProtocol
+        }
+    }
+    
+    func popViewController() {
+        self.dismiss() //Check if dismiss protocol assign so that calling protocol func become priority
+        if ((self.popViewProtocol) != nil) {
+            self.popViewProtocol.popView(self)
+        } else { //If no protocol assignment call popViewController manually
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func goBack() {
+        if ((self.navigationController) != nil) { //navigation pop priority
+            self.popViewController()
+        }
+        self.dismiss()
+    }
+
+}
--- Contacts/Classes/GKLibrary/GKView/GKViewController.swift
+++ Contacts/Classes/GKLibrary/GKView/GKViewController.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import UIKit
+
+class GKViewController : GKBasicViewController {
+    var activityIndicatorView = UIActivityIndicatorView(style: .gray)
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(self.handleRefresh(_:)), for: UIControl.Event.valueChanged)
+        return refreshControl
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicatorView)
+        
+        self.activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        self.activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func stopViewAnimation() {
+        DispatchQueue.main.async {
+            self.activityIndicatorView.stopAnimating()
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        
+    }
+    
+    func startViewAnimation() {
+        DispatchQueue.main.async {
+            self.activityIndicatorView.startAnimating()
+        }
+    }
+    
+    func showAlertWithTitleAndMessage(title: String, message: String, handler: ((UIAlertAction) -> Swift.Void)? = nil) {
+        let alert = UIAlertController.init(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: handler))
+        self.present(alert, animated: true, completion: {
+        })
+    }
+}
